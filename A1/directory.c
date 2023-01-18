3b7693e9706259733105a4134c66cc6f5df76c09
harrisiva <harrishiv6@gmail.com>
2023-01-18 16:52:27 -0500

    Added empty case blocks for printing contents of a directory and closing a directory

--- A1/directory.c
+++ A1/directory.c
@@ -67,7 +67,12 @@ int main() {
                 } else {
                     printf("Directory Changed Unsuccessful.\n");
                 }
+                break;
 
+            case '5': // Print contents of the working directory
+                break;
+
+            case '6': // Close the working directory
                 break;
 
             default:
@@ -77,34 +82,6 @@ int main() {
         }
     }
 
-
-
-
-    // create a directory
-    //mkdir("test", 0700); // 0700 is the mode, not sure what it exactly means but mkdir is the command we want, same in linux
-
-    // remove a directory
-    //rmdir("test");
-
-    // get current working directory
-    //char cwd[10000];
-    //getcwd(cwd, sizeof(cwd));
-    //printf("%s",cwd);
-
-    // change a directory (one level up current)
-    // char cwd[10000];
-    // getcwd(cwd, sizeof(cwd));
-    // printf("%s\n",cwd);
-
-    // chdir("..");
-
-    // char cwd2[10000];
-    // getcwd(cwd2, sizeof(cwd2));
-    // printf("%s\n",cwd2);
-
-
-
-    // Read a directory
     // Print contents of a directory onto the terminal
     // use listdir
 
