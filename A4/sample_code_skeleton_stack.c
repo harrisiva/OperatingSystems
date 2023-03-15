0af4d2cf7cf1a473383fc1c88a989ae75c3e0e28
harrisiva <harrishiv6@gmail.com>
2023-03-15 20:23:36 +0000

    Added the comments/notes

--- A4/sample_code_skeleton_stack.c
+++ A4/sample_code_skeleton_stack.c
@@ -1,18 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
-#define NUM_THREADS 	10
-typedef struct Node
-{
+#define NUM_THREADS 10
+
+// Declare the structure of the node
+typedef struct Node {
     int data;
     struct Node *next;
 } StackNode;
-StackNode *top;
-// function prototypes
-void push(int v, StackNode **top);
+
+StackNode *top; // initialize a gloabl stackNode pointer variable that points to the top of the stack
+// Function prototypes
+void push(int v, StackNode **top); // takes a pointer to a pointer
 int pop(StackNode **top);
-int is_empty(StackNode *top);
-// push function
+int is_empty(StackNode *top); // takes the pointer as is
+
+// Since top is the only global variable,
+// to prevent race conditions, we need to add mutex locks to
+// all the sections that make changes to access "top"
+
+// Push function
 void push(int v, StackNode **top)
 {
     StackNode *new_node;
@@ -22,7 +29,8 @@ void push(int v, StackNode **top)
     new_node->next = *top;
     *top = new_node;
 }
-// pop function
+
+// Pop function
 int pop(StackNode **top)
 {
 	StackNode *temp;
@@ -39,54 +47,68 @@ int pop(StackNode **top)
         return data;
     }
 }
-//Check if top is NULL
+
+//Check if top is NULL (reads from top, doesnt do any writing, does it still need the mutex lock?)
 int is_empty(StackNode *top) {
     if (top == NULL)
         return 1;
     else
         return 0;
 }
-// Thread's push function
-void* thread_push(void *args)
+
+// Thread's push function ??
+void* thread_push(void *args) // add mutex here or within the push and pull cause thats where the shared data (stack) is actually manipulated by each thread
 {
-	int i;
+	int i; // where does this get used? 
     int *threadId = (int *)args;
     push(*threadId + 1,&top);
     return NULL;
 }
+
 // Thread's pop function 
-void* thread_pop(){
+void* thread_pop(){ // add mutex here?
     pop(&top);
     return NULL;
 }
+
 // main function 
 int main(void)
 {
     StackNode *top = NULL;
-    pthread_t threads_push[NUM_THREADS];
+    // two pthread_t arrays for pushing and popping
+    pthread_t threads_push[NUM_THREADS]; 
     pthread_t threads_pop[NUM_THREADS];
+
     int thread_args[NUM_THREADS];
     int i, j;
+
+
+// race condition possibilites are from this section, starts here 
     // Creating push threads
     for(i=0;i<NUM_THREADS;i++){
-	thread_args[i] = i;
+	    thread_args[i] = i;
 	    pthread_create(&threads_push[i],NULL,thread_push, (void *)&thread_args[i]);
 	}
+
 	// Create Pop Threads
 	for(i=0;i<NUM_THREADS;i++){
         thread_args[i] = i;
 	    pthread_create(&threads_pop[i],NULL,thread_pop, NULL);
 	}
+// race condition possibilites ends here
+
 	// Join Push Threads
 	for(j = 0; j < NUM_THREADS; j++)
 	{
 	pthread_join(threads_push[j],NULL);
 	}
+
     // Join Pop Threads
 	for(j = 0; j < NUM_THREADS; j++)
 	{
 	pthread_join(threads_pop[j],NULL);
 	}
+
     pthread_exit(NULL);
 	return 0;
 }
