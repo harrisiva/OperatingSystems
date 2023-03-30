2c2a65c017e88a1b9b3d2d7e1b5c94eb239bc50e
harrisiva <harrishiv6@gmail.com>
2023-03-30 16:27:24 +0000

    Added psuedocode for banker

---
+++ A5/banker.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Standardized output messages (e.x. invalid input)
+
+int main(){
+    // arguments taken when invoked (argv) -- LAST ITERATION
+    //  create a vector/array with the number of resources (value) per resource types (location) (function?) -- LAST ITERATION
+    
+
+    // TODO:
+    // Initiative maximum array to the values based on the sampe in banker input file (function)
+
+    // Enter while loop (while input not Exit) to handle ds (bankers and other algorithms)
+    //      Assume input follows format 
+
+    //      If request, Check if request is under max number of resources per thread (function)
+    //      Check if request meets system safe critera (safety algorithm from ch.8) and print message in main (function)
+
+    //      If release, release resources and update DS accordingly
+
+    //      If status, print all arrays and matrices used (available, maximum, allocation, and need)
+
+    //      If run, execute the safe sequence based on the current state and all threads should run the same **function** code (just prints, no global/critical sections)
+
+    return 0;
+}
